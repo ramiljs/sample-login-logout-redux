8569aacf809449e9c952205d56621929adeb88a4
Ramil <89657343+kamalovvvv@users.noreply.github.com>
2022-09-11 14:17:30 +0500

    Update state.js

--- src/state.js
+++ src/state.js
@@ -1,51 +1,50 @@
 export const initialState = {
-  username: '',
-  password: '',
-  Loading: false,
-  error: '',
-  LoggedIn: false
+    username: '',
+    password: '',
+    Loading: false,
+    error: '',
+    LoggedIn: false
 }
 
 export const loginReducer = (state, action) => {
-  switch (action.type) {
-    case 'field': {
-      return {
-        ...state,
-        [action.field]: action.value
-      }
-    }
-    case 'login': {
-      return {
-        ...state,
-        Loading: true,
-        error: ''
-      }
-    }
-    case 'success': {
-      return {
-        ...state,
-        LoggedIn: true
-      }
-    }
-    case 'error': {
-      return {
-        ...state,
-        error: 'Incorrect username or password!',
-        Loading: false,
-        username: '',
-        password: ''
-      }
-    }
-    case 'logout': {
-      return {
-        ...state,
-        LoggedIn: false,
-        username: '',
-        password: ''
-      }
-    }
-
-    default:
-      return state
-  }
+    switch (action.type) {
+        case 'field': {
+            return {
+                ...state,
+                [action.field]: action.value
+            }
+        }
+        case 'login': {
+            return {
+                ...state,
+                Loading: true,
+                error: ''
+            }
+        }
+        case 'success': {
+            return {
+                ...state,
+                LoggedIn: true
+            }
+        }
+        case 'error': {
+            return {
+                ...state,
+                error: 'Incorrect username or password!',
+                Loading: false,
+                username: '',
+                password: ''
+            }
+        }
+        case 'logout': {
+            return {
+                ...state,
+                LoggedIn: false,
+                username: '',
+                password: ''
+            }
+        }
+        default:
+            return state
+        }
 }
