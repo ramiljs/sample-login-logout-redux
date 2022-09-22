b3155d0e9c8bdfcefc2ebaf48e853c48ef326e05
Ramil <89657343+kamalovvvv@users.noreply.github.com>
2022-09-22 16:56:39 +0500

    Update login.jsx

--- src/login.jsx
+++ src/login.jsx
@@ -3,11 +3,8 @@ import React from 'react';
 export async function login({ username, password }) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            if (username === 'ramil' && password === 'ramil') {
-                resolve()
-            } else {
-                reject()
-            }
+            if (username === 'ramil' && password === 'ramil') resolve()
+            else reject()
         }, 1000)
     })
 };
